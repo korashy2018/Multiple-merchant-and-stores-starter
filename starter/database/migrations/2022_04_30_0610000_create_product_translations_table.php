c4753d637dc506236376a3223c5a9d7e281293f2
korashy <a.ashraf.elgammal@hotmail.com>
2022-04-30 06:06:54 +0200

    created products migrations

---
+++ starter/database/migrations/2022_04_30_0610000_create_product_translations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_translations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('locale')->index();
+            $table->unique(['product_id', 'locale'],
+                'product_id_trans_with_locale');
+            $table->foreignId('product_id')->references('id')
+                ->on('products')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_translations');
+    }
+}
