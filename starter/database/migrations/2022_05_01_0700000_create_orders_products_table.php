3247cbc0ababce8ac3c79b4e1ad003bccdf6acc9
korashy <a.ashraf.elgammal@hotmail.com>
2022-05-01 06:15:57 +0200

    added orders migrations

---
+++ starter/database/migrations/2022_05_01_0700000_create_orders_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_products', function (Blueprint $table) {
+            $table->foreignId('order_id')->references('id')->on('orders');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->integer('quantity');
+            $table->float('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_products');
+    }
+}
